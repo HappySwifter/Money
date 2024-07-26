84e8232a3306270f4c81cbab52d621217db83765
Artem Valiev <artem.valiev@gmail.com>
2024-07-26 11:37:00 +0530

    history view duplicated transactions error fix

--- Money/Views/History/HistoryView.swift
+++ Money/Views/History/HistoryView.swift
@@ -15,9 +15,23 @@ private enum HistoryType: String, CaseIterable {
     case spending = "Spendings"
 }
 
-private enum PaginationState {
+private enum PaginationState: Equatable {
+    case idle
     case isLoading
     case error(error: Error)
+    
+    static func == (lhs: PaginationState, rhs: PaginationState) -> Bool {
+        switch (lhs, rhs) {
+        case (.idle, .idle):
+            return true
+        case (.isLoading, .isLoading):
+            return true
+        case (.error, .error):
+            return true
+        default:
+            return false
+        }
+    }
 }
 
 @MainActor
@@ -30,10 +44,13 @@ struct HistoryView: View {
     @State private var transactions = [MyTransaction]()
     @State private var groupedData = [TransactionsByDate]()
     
-    @State private var paginationState = PaginationState.isLoading
+    @State private var paginationState = PaginationState.idle
     @State private var allDataCount = 0
     
-    private var isMoreDataAvailable: Bool { transactions.count < allDataCount }
+    private var isMoreDataAvailable: Bool {
+        assert(transactions.count <= allDataCount)
+        return transactions.count < allDataCount
+    }
     
     var body: some View {
         VStack {
@@ -45,29 +62,38 @@ struct HistoryView: View {
                     }
                 } label: {}
             }
-            List {
-                ForEach(groupedData, id: \.date) { group in
-                    Section {
-                        ForEach(group.transactions) { transaction in
-                            if transaction.isIncome {
-                                IncomeView(transaction: transaction)
-                            } else if (transaction.destination?.isAccount ?? false) {
-                                TransferView(transaction: transaction)
-                            } else {
-                                SpengingView(transaction: transaction)
+            if groupedData.isEmpty {
+                Spacer()
+                Text("No data available")
+                    .font(.title)
+                Spacer()
+            } else {
+                List {
+                    ForEach(groupedData, id: \.date) { group in
+                        Section {
+                            ForEach(group.transactions) { transaction in
+                                if transaction.isIncome {
+                                    IncomeView(transaction: transaction)
+                                } else if (transaction.destination?.isAccount ?? false) {
+                                    TransferView(transaction: transaction)
+                                } else {
+                                    SpengingView(transaction: transaction)
+                                }
                             }
+                            .onDelete(perform: { indexSet in
+                                if let index = indexSet.first {
+                                    deleteTransaction(at: index, date: group.date)
+                                }
+                            })
+                            .padding(.vertical, 5)
+                        } header: {
+                            Text(group.date.historyDateString)
+                                .font(.title3)
                         }
-                        .onDelete(perform: { indexSet in
-                            deleteTransaction(at: indexSet, date: group.date)
-                        })
-                        .padding(.vertical, 5)
-                    } header: {
-                        Text(group.date.historyDateString)
-                            .font(.title3)
                     }
-                }
-                if isMoreDataAvailable {
-                    lastRowView
+                    if isMoreDataAvailable {
+                        lastRowView
+                    }
                 }
             }
         }
@@ -86,6 +112,8 @@ struct HistoryView: View {
     var lastRowView: some View {
         ZStack(alignment: .center) {
             switch paginationState {
+            case .idle:
+                Color.clear
             case .isLoading:
                 ProgressView()
             case .error(let error):
@@ -106,15 +134,17 @@ struct HistoryView: View {
                 self.allDataCount = try await dataHandler.getTransactionsCount(with: predicate)
             }
         } catch {
-            print(error)
+            paginationState = .error(error: error)
         }
     }
     
     private func fetchTransactions(type: HistoryType, offset: Int) async {
+        guard isMoreDataAvailable, paginationState == .idle else {
+            return
+        }
+        paginationState = .isLoading
         print("fetchTransactions", offset)
         do {
-            paginationState = .isLoading
-            //            fetchDescriptor.propertiesToFetch =
             if let dataHandler = await dataHandler() {
                 let predicate = getPredicateFor(type: type)
                 let sortBy = [SortDescriptor(\MyTransaction.date, order: .reverse)]
@@ -125,6 +155,7 @@ struct HistoryView: View {
                     fetchLimit: fetchChunkSize)
                 transactions.append(contentsOf: newChunk)
                 groupedData = group(transactions: transactions)
+                paginationState = .idle
             }
         } catch {
             paginationState = .error(error: error)
@@ -144,18 +175,18 @@ struct HistoryView: View {
         }
     }
     
-    private func deleteTransaction(at offsets: IndexSet, date: Date) {
+    private func deleteTransaction(at index: Int, date: Date) {
         Task { @MainActor in
             do {
-                if let trans = groupedData.first(where: { $0.date == date }) {
-                    for i in offsets {
-                        let model = trans.transactions[i]
-                        transactions.removeAll(where: { $0.id == model.id })
-                        try await dataHandler()?.undo(transaction: model)
-                    }
-                    groupedData = group(transactions: transactions)
-                    try await expensesService.calculateSpent()
+                guard let trans = groupedData.first(where: { $0.date == date }) else {
+                    return
                 }
+                let model = trans.transactions[index]
+                try await dataHandler()?.undo(transaction: model)
+                await fetchCount(type: selectedTransType)
+                transactions.removeAll(where: { $0.id == model.id })
+                groupedData = group(transactions: transactions)
+                try await expensesService.calculateSpent()
             } catch let error as DataProviderError {
                 print("ERROR: ", error.rawValue)
                 assert(false)
@@ -179,10 +210,6 @@ struct HistoryView: View {
             .sorted { $0.date > $1.date }
     }
 }
-
-//#Preview {
-//    HistoryView()
-//}
 
 extension Date {
     var omittedTime: Date {
